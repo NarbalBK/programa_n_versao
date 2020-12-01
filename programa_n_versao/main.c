fc26f3881dfaaf7abdf2f2bc701b0bc3a674027f
periclesnarbal <periclesnarbal@gmail.com>
2020-12-01 20:00:57 -0300

    Initial Commit

---
+++ programa_n_versao/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  programa_n_versao
+//
+//  Created by Péricles Narbal on 01/12/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
